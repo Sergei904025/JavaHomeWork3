a0162106cd0f70916642bcd4f1d9d7bf5eb85fc6
Sergei904025 <105377873+Sergei904025@users.noreply.github.com>
2023-08-30 11:59:18 +0600

    Task № 1

--- src/main/java/HW3.java
+++ src/main/java/HW3.java
@@ -1,17 +1,22 @@
 public class HW3 {
 
     public static void main(String[] args) {
-        int[] arr = {10, 3, 1, 100, 7, 4, 11};
 
-// Предположим, что нулевой элемент максимальный
-        int max = arr[0];
+        System.out.println("Задача №1");
 
-// В цикле начинаем с первой ячейки
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+        int[] numbers = {2, 5, 13, 7, 6, 4};
+        int size = 6;
+        double avg = 0;
+        int sum = 0;
+        int index = 0;
+        while (index < size) {
+            sum = sum + numbers[index];
+            index++;
         }
-        System.out.println(max); // => 11
+        avg = (double) (1.0 * sum / size);
+
+        System.out.println("среднее арифметическое массива numbers равно " + avg);
     }
 }
+
+
